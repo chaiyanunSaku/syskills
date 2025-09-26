3e2ae960b83b3daed3a725c2d8580edf176b3179
onglee <ongleesaku@gmail.com>
2025-09-26 18:42:26 +0700

    Update organize.sh for ex3

--- ex3/organize.sh
+++ ex3/organize.sh
@@ -1,6 +1,39 @@
+#!/bin/bash
 # A nano script
 
-echo "March 7th"
-ls
-pwd
-echo "Done!"
+echo "March 7th is doing the work for you"
+
+#for file in unorganized/*; do
+#    echo "Found file: $file"
+#done
+
+#for file in unorganized/*; do
+#    filename=$(basename "$file")
+#    extension="${filename##*.}"
+#    echo "File: $filename, Extension: $extension"
+#done
+
+for file in unorganized/*; do
+    filename=$(basename "$file")
+    extension="${filename##*.}"
+
+    # we will decide folder based on extension
+    if [[ "$extension" == "txt" ]]; then
+    	folder="texts"
+    elif [[ "$extension" == "jpg" || "$extension" == "png" ]]; then
+    	folder="images"
+    elif [[ "$extension" == "pdf" ]]; then
+        folder="documents"
+    else
+    	folder="others"
+    fi
+
+    # create folder if it doesn't exist
+    mkdir -p "$folder"
+    mv "$file" "$folder/"
+
+    # print logs
+    echo "Moving $filename to %folder/"
+done
+
+echo "March finished the work!"
